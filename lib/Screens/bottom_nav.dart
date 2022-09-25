d7c49c1ed5ff635c1876c81fbb9db232577436dd
michael-stac <mickymouze778@gmail.com>
2022-09-25 02:01:17 +0100

    add some code

---
+++ lib/Screens/bottom_nav.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
